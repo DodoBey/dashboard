550f9a4506cd1f7e7aa49a233d7076a3dfe86368
Dogukan Yigiter <dogukanyigiter@Dogukans-MacBook-Air.local>
2024-03-08 03:20:09 -0800

    Boilarplate for the first Vue app of mine

---
+++ src/components/TheWelcome/TheWelcome.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Personal Dashboard</h1>
+        <p>The dashboard designed for myself to keep things organized.</p>
+        <button>Let's Start</button>
+    </div>
+</template>
