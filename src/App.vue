9cb693364df1ea33a11629a95a2a429241f03c7a
Dogukan Yigiter <dogukanyigiter@Dogukans-MacBook-Air.local>
2024-03-10 03:53:44 -0700

    Welcome page is done, working on setup page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from './components/TheWelcome/TheWelcome.vue';
-</script>
-
 <template>
-  <TheWelcome />
+  <div class="outer-container rounded-xl bg-blue-grey-lighten-2">
+    <router-view></router-view>
+  </div>
 </template>
 
 
+<style scoped>
+.outer-container {
+  margin: 30px;
+  max-height: calc(100vh - 60px);
+  max-width: calc(100vw - 60px);
+  overflow: hidden;
+}
+</style>
--- src/components/TheWelcome/TheWelcome.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>Personal Dashboard</h1>
-        <p>The dashboard designed for myself to keep things organized.</p>
-        <button>Let's Start</button>
-    </div>
-</template>
