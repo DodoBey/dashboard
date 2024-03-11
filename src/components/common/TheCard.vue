6ae4cb0037652266b511215c28c91c92c8f0f9ca
Dogukan Yigiter <dogukanyigiter@Dogukans-MacBook-Air.local>
2024-03-10 21:52:04 -0700

    Fixed the issue with the vuetify, resetted the branch

---
+++ src/components/common/TheCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <v-card variant="tonal">
+        TEST
+    </v-card>
+</template>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,28 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Composables
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { myCustomDarkTheme, myCustomLightTheme } from "@/styles/theme";
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "myCustomDarkTheme",
+    themes: {
+      myCustomDarkTheme,
+      myCustomLightTheme,
+    },
+  },
+});
